9093236dcace8078f4e477dd0e6a899a5f9b7db2
Tanvir-51 <tanvir19ahmed@gmail.com>
2021-10-13 17:56:05 +0600

    first commit

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,10 @@
+import React from "react";
+import neon from "../../../images/services/aaron-huber-KxeFuXta4SE-unsplash.png";
+
+const services = [{ name: "Replace Tire", price: 2000, time: 2, image: neon }];
+
+const Services = () => {
+  return <div></div>;
+};
+
+export default Services;
